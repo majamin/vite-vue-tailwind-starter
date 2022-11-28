5ec1bfa89909e73a6f8c37e2ef346089ee550ee0
Marian Minar <majamin@gmail.com>
2022-11-28 07:41:13 -0800

    table template can receive generic data

--- src/components/full/TableTemplate.vue
+++ src/components/full/TableTemplate.vue
@@ -1,53 +1,54 @@
 <script setup lang="ts">
+const data = {
+  header: ["model", "mpg", "hp", "gear"],
+  values: [
+    ["Mazda RX4", 21.0, 110, 4],
+    ["Mazda RX4 Wag", 21.0, "NA", 4],
+    ["Datsun 710", 22.8, 93, 4],
+    ["Hornet 4 Drive", 21.4, 110, 3],
+    ["Valiant", 18.1, 105, 3],
+  ],
+};
 </script>
 <template>
   <div class="relative overflow-x-auto">
-    <table class="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-      <thead
-        class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400"
-      >
+    <table>
+      <thead>
         <tr>
-          <th scope="col" class="py-3 px-6">Product name</th>
-          <th scope="col" class="py-3 px-6">Color</th>
-          <th scope="col" class="py-3 px-6">Category</th>
-          <th scope="col" class="py-3 px-6">Price</th>
+          <th v-for="heading in data.header" scope="col">
+            {{ heading }}
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr class="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-          <th
-            scope="row"
-            class="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white"
-          >
-            Apple MacBook Pro 17"
-          </th>
-          <td class="py-4 px-6">Sliver</td>
-          <td class="py-4 px-6">Laptop</td>
-          <td class="py-4 px-6">$2999</td>
-        </tr>
-        <tr class="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-          <th
-            scope="row"
-            class="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white"
-          >
-            Microsoft Surface Pro
-          </th>
-          <td class="py-4 px-6">White</td>
-          <td class="py-4 px-6">Laptop PC</td>
-          <td class="py-4 px-6">$1999</td>
-        </tr>
-        <tr class="bg-white dark:bg-gray-800">
-          <th
-            scope="row"
-            class="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white"
-          >
-            Magic Mouse 2
-          </th>
-          <td class="py-4 px-6">Black</td>
-          <td class="py-4 px-6">Accessories</td>
-          <td class="py-4 px-6">$99</td>
+        <tr v-for="row in data.values">
+          <th scope="row">{{ row[0] }}</th>
+          <td>{{ row[1] }}</td>
+          <td>{{ row[2] }}</td>
+          <td>{{ row[3] }}</td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
+
+<style scoped lang="postcss">
+table {
+  @apply w-full text-left text-sm text-gray-500 dark:text-gray-400;
+}
+thead {
+  @apply text-xs uppercase border-b-4 text-gray-700 dark:bg-gray-700 dark:text-gray-400;
+}
+thead > tr > th {
+  @apply py-3 px-6;
+}
+tbody > tr {
+  @apply border-b bg-white dark:border-gray-700 dark:bg-gray-800;
+}
+tbody > tr > th {
+  @apply whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white;
+}
+tbody > tr > td {
+  @apply py-4 px-6;
+}
+</style>
