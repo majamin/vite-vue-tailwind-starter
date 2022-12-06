57858e0f5acc88a52960f98d1e054e2dd1ab437e
Marian Minar <majamin@gmail.com>
2022-12-05 16:29:01 -0800

    update base components

--- src/components/Base/BaseButton.vue
+++ src/components/Base/BaseButton.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-const { customStyle } = defineProps<{ customStyle: string }>();
-</script>
 <template>
-  <button type="button" :class="customStyle ? customStyle : 'bg-slate-500 text-slate-100' " class="min-w-[100px] text-white">
+  <button type="button" class="min-w-[100px]">
     <!-- Unnamed, default slot with default-->
     <slot>Submit</slot>
   </button>
